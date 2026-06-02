0c3e894aff94f16f7a0d738f71908f8c52edc9bd
R4aveen <bas.rojasl@duocuc.cl>
2026-06-02 02:18:13 -0400

    feat: add src/components/layout/Footer/Footer.tsx

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,74 @@
+import { type FC, type HTMLAttributes, type ReactNode } from "react";
+import classNames from "classnames";
+import useTheme from "@/hooks/useTheme";
+
+interface IFooterLeftProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+export const FooterLeft: FC<IFooterLeftProps> = (props) => {
+  const { children, className, ...rest } = props;
+
+  return (
+    <div
+      data-component-name="Footer/FooterLeft"
+      className={classNames("d-flex align-items-center gap-3", className)}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+FooterLeft.displayName = "FooterLeft";
+
+interface IFooterRightProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+export const FooterRight: FC<IFooterRightProps> = (props) => {
+  const { children, className, ...rest } = props;
+
+  return (
+    <div
+      data-component-name="Footer/FooterRight"
+      className={classNames(
+        "d-flex align-items-center gap-3 ms-auto",
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+FooterRight.displayName = "FooterRight";
+
+interface IFooterProps extends HTMLAttributes<HTMLElement> {
+  children: ReactNode;
+  className?: string;
+}
+const Footer: FC<IFooterProps> = (props) => {
+  const { children, className, ...rest } = props;
+  const theme = useTheme();
+
+  const hasBorder = theme.borderWidth !== "border-0";
+
+  return (
+    <footer
+      data-component-name="Footer"
+      className={classNames(
+        "d-flex justify-content-between p-3 mt-auto bg-body-tertiary small",
+        {
+          "border-top": hasBorder,
+          
+          [theme.borderWidth]: hasBorder,
+        },
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </footer>
+  );
+};
+Footer.displayName = "Footer";
+
+export default Footer;
